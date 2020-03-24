5334d51a73df4f9e7fe5677f0ecf0f3f5e9599db
EC2 Default User <ec2-user@ip-172-31-25-180.us-west-2.compute.internal>
2020-03-24 04:29:14 +0000

    fix cucumber cases for login and password reset

--- features/step_definitions/help_steps.rb
+++ features/step_definitions/help_steps.rb
@@ -57,7 +57,8 @@ Given (/^a user$/) do |table|
   @user = User.create!(
     admin: false,
     email: data['Email'] || 'user1@test.com',
-    name: data['Name'] || 'TestUser',
+    firstname: data['Firstname'] || 'Test',
+    lastname: data['Lastname'] || 'User',
     password: data['Password'] || 'password',
     uin: data['UIN'] || '111111111',
     year: data['Year'] || '2018',
@@ -71,7 +72,8 @@ Given (/^an admin$/) do |table|
   @user = User.create!(
     admin: true,
     email: data['Email'] || 'admin1@test.com',
-    name: data['Name'] || 'TestAdmin',
+    firstname: data['Firstname'] || 'Test',
+    lastname: data['Lastname'] || 'Admin',
     password: data['Password'] || 'password',
     uin: data['UIN'] || '123123123',
     year: data['Year'] || '2018',
@@ -124,11 +126,12 @@ end
 
 Given(/^I fill in the following details:$/) do |table|
   data = table.rows_hash
-  fill_in 'Name', with: data['Name']
+  fill_in 'First Name', with: data['Firstname']
+  fill_in 'Last Name', with: data['Lastname']
   fill_in 'UIN', with: data['UIN']
   fill_in 'Email', with: data['Email']
-  fill_in 'Password', with: data['Password']
-  fill_in 'Confirmation', with: data['Confirmation']
+  fill_in 'Password', with: data['Password'], match: :first
+  fill_in 'Confirm Password', with: data['Confirm Password']
   select data['Semester'], from: 'Semester'
   select data['Year'], from: 'Year'
   select data['Course'], from: 'Course'
@@ -167,7 +170,8 @@ end
 
 Then(/^I fill the updated details:$/) do |table|
   data = table.rows_hash
-  fill_in 'Name', with: data['Name']
+  fill_in 'First Name', with: data['Firstname']
+  fill_in 'Last Name', with: data['Lastname']
   fill_in 'Email', with: data['Email']
   fill_in 'UIN', with: data['UIN']
   fill_in 'New Password', with: data['New Password']
